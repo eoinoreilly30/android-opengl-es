256261ca79471a523990823c5197390edb1719b8
eoin <eoinoreilly30>
2020-04-17 16:47:00 +0100

    f

--- app/src/main/java/com/example/assignment_2/OpenGLES20Activity.java
+++ app/src/main/java/com/example/assignment_2/OpenGLES20Activity.java
@@ -1,13 +1,24 @@
 package com.example.assignment_2;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.content.Context;
+import android.util.Log;
 
-public class OpenGLES20Activity extends Activity {
+import java.util.Arrays;
 
-    private GLSurfaceView gLView;
+public class OpenGLES20Activity extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager = null;
+    private Sensor accelerometer = null;
+    private Sensor magnetometer = null;
+    private float[] gravity = new float[3];
+    private float[] linearAcceleration = new float[3];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,21 +26,63 @@ public class OpenGLES20Activity extends Activity {
 
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity.
-        gLView = new MyGLSurfaceView(this);
+        GLSurfaceView gLView = new MyGLSurfaceView(this);
         setContentView(gLView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        assert sensorManager != null;
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        Arrays.fill(gravity, 0);
+        Arrays.fill(linearAcceleration, 0);
     }
 
-    class MyGLSurfaceView extends GLSurfaceView {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        private final MyGLRenderer renderer;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
-        public MyGLSurfaceView(Context context){
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final float alpha = (float) 0.8;
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+            linearAcceleration[0] = event.values[0] - gravity[0];
+            linearAcceleration[1] = event.values[1] - gravity[1];
+            linearAcceleration[2] = event.values[2] - gravity[2];
+
+            Log.i("tag", "x: " + linearAcceleration[0] + " y: " + linearAcceleration[1]
+                    + " z: " + linearAcceleration[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    static class MyGLSurfaceView extends GLSurfaceView {
+
+        public MyGLSurfaceView(Context context) {
             super(context);
 
             // Create an OpenGL ES 2.0 context
             setEGLContextClientVersion(2);
 
-            renderer = new MyGLRenderer();
+            MyGLRenderer renderer = new MyGLRenderer();
 
             // Set the Renderer for drawing on the GLSurfaceView
             setRenderer(renderer);
